4ed04820233c32afed02558a2e53d7aef504b3c8
Kaartokallio Ohto <ohto.kaartokallio@aalto.fi>
2022-12-13 15:24:50 +0200

    viimeinen tehtävä

--- osa1/t_1.12/src/App.js
+++ osa1/t_1.12/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [points,setPoints] = useState([])
+
+
+  if(points.length === 0){
+    setPoints(Array(anecdotes.length).fill(0))
+  }
+
+  const handleSelection = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+
+  const handleVotes = () => {
+    const copy = [...points]
+    copy[selected]+=1
+    setPoints(copy)
+    }
+
+  const handleTop = () => {
+    var max = points[0];
+    var maxIndex = 0;
+
+    for (var i = 1; i < points.length; i++) {
+        if (points[i] > max) {
+            maxIndex = i;
+            max = points[i];
+        }
+    }
+    return [anecdotes[maxIndex], points[maxIndex]];
 }
 
-export default App;
+  const [mostVoted, voteAmount] = handleTop();
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]} <br/>
+      has {points[selected]} votes<br/>
+      <Button handleClick = {handleVotes} text = "vote"></Button>
+      <Button handleClick = {handleSelection} text = "next anedctote"></Button>
+      <h1>Anedctote with most votes</h1>
+      {mostVoted}<br/>
+      has {voteAmount} votes
+    </div>
+  )
+}
+
+export default App
