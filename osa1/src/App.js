0d20fc0fd2e3c54bb1b1f723ac70fec06e0ded5d
Kaartokallio Ohto <ohto.kaartokallio@aalto.fi>
2022-12-13 02:17:13 +0200

    tehtävät 1.1-1.6

---
+++ osa1/src/App.js
@@ -0,0 +1,59 @@
+const Header = (props) => (
+  <>
+    <h1>{props.name}</h1>
+  </>
+)
+const Content = (props) => {
+  console.log(props)
+  console.log(props.parts[0])
+  return(
+  <div>
+    <Part part = {props.parts[0].name} exercise = {props.parts[0].exercises}/>
+    <Part part = {props.parts[1].name} exercise = {props.parts[1].exercises}/>
+    <Part part = {props.parts[2].name} exercise = {props.parts[2].exercises}/>
+  </div> 
+)}
+
+const Total = (props) => (
+  <>
+  <p>Number of exercises {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}</p>
+  </>
+)
+
+const Part = (props) => (
+  <p>
+  {props.part} {props.exercise}
+</p>
+)
+
+// MAIN APP
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header name = {course.name}/>
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
+    </div>
+  )
+}
+
+export default App
